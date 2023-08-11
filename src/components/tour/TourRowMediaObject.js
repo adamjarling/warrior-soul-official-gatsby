074a5518e74418c1d6fb36b53902a1e55203d1a3
Adam Joseph Arling <aarling@gmail.com>
2023-08-11 15:32:31 -0500

    Safari date display bug; Add other Germany dates: Facebook event links

--- src/components/tour/TourRowMediaObject.js
+++ src/components/tour/TourRowMediaObject.js
@@ -4,13 +4,12 @@ import moment from "moment";
 
 const TourRowMediaObject = ({ dates }) => {
   return dates.map((date) => {
-    const { datetime, description, id, lineup, title, url, venue } = date;
+    const { datetime, description, facebook, id, lineup, title, url, venue } =
+      date;
 
     return (
-      <article key={id} className="notification is-dark">
-        <h3 className="title">
-          {moment(datetime).format("dddd MMMM D, YYYY")}
-        </h3>
+      <article key={datetime} className="notification is-dark">
+        <h3 className="title">{moment(datetime).format("ddd, MMM DD YYYY")}</h3>
         <h4 className="subtitle has-text-warning">{title}</h4>
         <div className="columns">
           <div className="column one-third">
@@ -29,25 +28,24 @@ const TourRowMediaObject = ({ dates }) => {
           </div>
           <div className="column one-third">
             {url && (
-              <div className="buttons is-right">
+              <div className="buttons is-right ">
                 <ExternalLink href={url} className="button">
                   Tickets
                 </ExternalLink>
-                {/* <ExternalLink
-                href={url}
-                className="button is-primary is-outlined"
-              >
-                Notify Me
-              </ExternalLink> */}
+                {facebook && (
+                  <ExternalLink href={url} className="button is-ghost mt-3">
+                    Facebook Event
+                  </ExternalLink>
+                )}
               </div>
             )}
           </div>
         </div>
-        <h5 className="is-uppercase is-size-7 has-text-warning">Line up</h5>
+        {/* <h5 className="is-uppercase is-size-7 has-text-warning">Line up</h5>
         {lineup.map((item) => (
           <p>{item}</p>
-        ))}
-        <div className="is-hidden-touch">
+        ))} */}
+        {/* <div className="is-hidden-touch">
           <h5
             className="is-uppercase is-size-7 has-text-warning"
             style={{ paddingTop: "1rem" }}
@@ -55,7 +53,7 @@ const TourRowMediaObject = ({ dates }) => {
             Description
           </h5>
           <p>{description}</p>
-        </div>
+        </div> */}
       </article>
     );
   });
